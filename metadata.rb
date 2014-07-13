252203ebf352e1563afd02857a78234bd564c2c2
Maxim Kremenev <ezo@kremenev.com>
2014-07-13 10:00:38 +0700

    init commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'influxdb'
+maintainer       'Maxim Kremenev'
+maintainer_email 'ezo@kremenev.com'
+license          'All rights reserved'
+description      'Installs/Configures influxdb'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: influxdb
+# Recipe:: default
+#
+# Copyright 2014, Maxim Kremenev
+#
+# All rights reserved - Do Not Redistribute
+#
