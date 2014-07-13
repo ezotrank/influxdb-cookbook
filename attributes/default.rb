8bd4b593f6b65c106c40c788ca716730a8d20ae4
Maxim Kremenev <ezo@kremenev.com>
2014-07-13 10:43:03 +0700

    add default recipe

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['influxdb']['version'] = '0.7.3-1'
+default['influxdb']['rpm_url'] = "http://s3.amazonaws.com/influxdb/influxdb-#{node['influxdb']['version']}.x86_64.rpm"
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+remote_file "#{Chef::Config[:file_cache_path]}/influxdb.rpm" do
+  source node['influxdb']['version']
+  action :create
+end
+
+rpm_package "influxdb" do
+  source "#{Chef::Config[:file_cache_path]}/influxdb.rpm"
+  action :install, :upgdate
+  not_if "influxdb -v|grep 'InfluxDB v#{node['influxdb']['version']}'"
+end
+
+service "influxdb" do
+  action :start, :enable
+end
